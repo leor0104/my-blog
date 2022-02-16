caaeb65d87d64e3b4cfc8e7b6929ae8ecfa7db0a
leor0104 <luisortiz141999@gmail.com>
2022-02-16 18:45:26 -0500

    Add social networks

---
+++ components/ButtonGroup/ButtonGroup.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+const ButtonGroup = () => (
+  <Button.Group>
+    <Button icon='github' href="https://github.com/leor0104"/>
+    <Button icon='mail' />
+    <Button icon='linkedin' />
+  </Button.Group>
+)
+
+export default ButtonGroup
