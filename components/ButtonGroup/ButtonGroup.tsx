d39cf0b5dad059db4c131c1d1bb472df34a48361
leor0104 <luisortiz141999@gmail.com>
2022-02-17 18:52:42 -0500

    Add links

--- components/ButtonGroup/ButtonGroup.tsx
+++ components/ButtonGroup/ButtonGroup.tsx
@@ -4,8 +4,8 @@ import { Button } from 'semantic-ui-react'
 const ButtonGroup = () => (
   <Button.Group>
     <Button icon='github' href="https://github.com/leor0104"/>
-    <Button icon='mail' />
-    <Button icon='linkedin' />
+    <Button icon='dribbble' href="https://dribbble.com/Indev-Leor"/>
+    <Button icon='linkedin' href="https://www.linkedin.com/in/luis-enrique-ortiz-rivera-96046b214/"/>
   </Button.Group>
 )
 
