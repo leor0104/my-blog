8619abc2650e52cac01745c226e7b86dcabb5397
leor0104 <luisortiz141999@gmail.com>
2022-02-22 17:16:08 -0500

    Add styles Header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,16 +1,27 @@
+import { Divider } from "semantic-ui-react";
 
 type HeaderProps = {
     title: string;
 }
 
-const Header = ({title}: HeaderProps) => {
+const Header = ({ title }: HeaderProps) => {
     return (
-      <div>
-      <p>
-          {title}
-      </p>
-      <hr/>
-      </div>
-      )
+        <div>
+            <p>
+                {title}
+            </p>
+            <Divider fitted />
+            <style jsx>
+                {`
+                    p{
+                        margin-bottom: 0px;
+                        font-size: 0.9em;
+                        color: grey;
+                        margin-bottom: 8px
+                    }
+                `}
+            </style>
+        </div>
+    )
 }
 export default Header;
