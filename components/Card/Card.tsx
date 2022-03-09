f279dd70e0a2bf1283afc51978f83330f1f1c2a3
leor0104 <luisortiz141999@gmail.com>
2022-03-08 20:18:01 -0500

    Change code

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Card, Icon} from 'semantic-ui-react'
-import Image from 'next/Image';
+import Image from 'next/Image'
 import styles from '@components/Card/Card.module.css'
 
 const services = [{
