b9aee3d901c08ad129670b7abf98bfb2d595e543
leor0104 <luisortiz141999@gmail.com>
2022-03-09 22:55:12 -0500

    Functional form

--- components/Form/FormContact/FormContact.tsx
+++ components/Form/FormContact/FormContact.tsx
@@ -1,59 +1,72 @@
-import React from 'react'
-import { Form, Input, TextArea, Button, Select } from 'semantic-ui-react'
+import React, { useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
+import { Form, Input, TextArea, Button, Select, Message } from 'semantic-ui-react'
+import MessageSubmit from '@components/Message/MessageSubmit';
 
-const genderOptions = [
-  { key: 'm', text: 'Male', value: 'male' },
-  { key: 'f', text: 'Female', value: 'female' },
-  { key: 'o', text: 'Other', value: 'other' },
-]
+function FormContact() {
 
-const FormContact = () => (
-  <Form>
-    <Form.Group widths='equal'>
+  const [result, setResult] = useState(false);
+
+  const sendEmail = (e: any) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_82naxwm', 'template_2cjqgsf', e.target, 'NUvoAyzA4Wx_2vGfo')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+    e.target.reset();
+    setResult(true);
+  };
+
+  return (
+    <Form onSubmit={sendEmail} success>
+      <Form.Group widths='equal' onClick={() => setResult(false)}>
+        <Form.Field
+          id='form-input-control-first-name'
+          control={Input}
+          label='First name'
+          placeholder='First name'
+          name='first_name'
+        />
+        <Form.Field
+          id='form-input-control-last-name'
+          control={Input}
+          label='Last name'
+          placeholder='Last name'
+          name='last_name'
+        />
+        <Form.Field
+          id='form-input-control-phone'
+          control={Input}
+          label='Phone'
+          placeholder='(xxx)'
+          name='phone'
+        />
+      </Form.Group>
       <Form.Field
-        id='form-input-control-first-name'
+        id='form-textarea-control-opinion'
+        control={TextArea}
+        label='Message'
+        placeholder='Message'
+        name='message'
+      />
+      <Form.Field
+        id='form-input-control-error-email'
         control={Input}
-        label='First name'
-        placeholder='First name'
-      />
-      <Form.Field
-        id='form-input-control-last-name'
-        control={Input}
-        label='Last name'
-        placeholder='Last name'
-      />
-      <Form.Field
-        control={Select}
-        options={genderOptions}
-        label={{ children: 'Gender', htmlFor: 'form-select-control-gender' }}
-        placeholder='Gender'
-        search
-        searchInput={{ id: 'form-select-control-gender' }}
-      />
-    </Form.Group>
-    <Form.Field
-      id='form-textarea-control-opinion'
-      control={TextArea}
-      label='Opinion'
-      placeholder='Opinion'
-    />
-    <Form.Field
-      id='form-input-control-error-email'
-      control={Input}
-      label='Email'
-      placeholder='joe@schmoe.com'
+        label='Email'
+        placeholder='loe@schmoe.com'
+        name='email'
       // error={{
       //   content: 'Please enter a valid email address',
       //   pointing: 'below',
       // }}
-    />
-    <Form.Field
-      id='form-button-control-public'
-      control={Button}
-      content='Confirm'
-      label='Label with htmlFor'
-    />
-  </Form>
-)
+      />
+      <Form.Button content='Submit' />
+      {result ? <MessageSubmit /> : null}
+    </Form>
+  )
+}
 
 export default FormContact
